1648baa68c8cb3a29f66368f865de66d95ac07dd
Ariel Gerstein <ariel.ger@hotmail.com>
2019-07-16 01:52:17 -0300

    Add is article bookmarked micro-interaction

--- src/components/NewsList/NewsItem.js
+++ src/components/NewsList/NewsItem.js
@@ -1,11 +1,15 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { css, keyframes } from "styled-components";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+
+import { ReactComponent as BookmarkIconRegular } from "./bookmark-icon.svg";
+import { ReactComponent as BookmarkIconSolid } from "./bookmark-icon-solid.svg";
 
 const ArticleWrapper = styled.article`
   display: flex;
   flex-direction: column;
   margin-bottom: 36px;
+  position: relative;
 
   .article-image {
     width: 100%;
@@ -27,6 +31,72 @@ const ArticleWrapper = styled.article`
 
   .description {
     margin: 0;
+  }
+`;
+
+const pulseAnimation = keyframes`
+  from {
+    transform: scale(1)
+  }
+
+  50% {
+    transform: scale(1.05);
+  }
+
+  to {
+    transform: scale(1);
+  }
+`;
+
+const SaveToBookmarks = styled.button`
+  background: transparent;
+  border: none;
+  position: absolute;
+  cursor: pointer;
+  top: 5px;
+  right: 5px;
+  height: 30px;
+  max-width: 30px;
+  color: #fff;
+  padding: 0;
+  font-weight: 500;
+  border-radius: 50%;
+  white-space: nowrap;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: inline-flex;
+  justify-content: flex-end;
+  align-items: center;
+  overflow: hidden;
+  transition: all 0.4s;
+
+  :focus {
+    outline: none;
+  }
+
+  &:hover {
+    max-width: 160px;
+    border-radius: 9999px;
+  }
+
+  ${({ active }) =>
+    active &&
+    css`
+      background-color: rgb(251, 94, 125);
+      color: #fff;
+      animation: ${pulseAnimation} 0.4s forwards;
+    `}
+
+  span {
+    padding: 0 4px 0 14px;
+    display: inline-block;
+    height: 30px;
+    line-height: 30px;
+  }
+
+  svg {
+    flex-shrink: 0;
+    width: 12px;
+    padding: 0 9px;
   }
 `;
 
@@ -57,6 +127,8 @@ export default function NewsItem({
   urlToImage,
   isPlaceholder = false
 }) {
+  const [isBookmark, setIsBookmark] = useState(false);
+
   return (
     <ArticleWrapper key={url}>
       {isPlaceholder ? (
@@ -69,6 +141,17 @@ export default function NewsItem({
         </>
       ) : (
         <>
+          <SaveToBookmarks
+            onClick={() => {
+              setIsBookmark(!isBookmark);
+            }}
+            active={isBookmark}
+          >
+            <span>
+              {isBookmark ? `Saved to bookmarks` : `Save to bookmark`}
+            </span>
+            {isBookmark ? <BookmarkIconSolid /> : <BookmarkIconRegular />}
+          </SaveToBookmarks>
           <a href={url}>
             {urlToImage ? (
               <LazyLoadImage
